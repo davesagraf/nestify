66fedc73e83dfd7dcaa118fe74985f641ead9aea
dave <mrdavidsaguev@proton.me>
2022-10-11 12:16:55 +0100

    ignore dist foler on commits

--- dist/db/migrations/1665416315263-UserEmailPassword.d.ts
+++
@@ -1,5 +0,0 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
-export declare class UserEmailPassword1665416315263 implements MigrationInterface {
-    up(queryRunner: QueryRunner): Promise<void>;
-    down(queryRunner: QueryRunner): Promise<void>;
-}
