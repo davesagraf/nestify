c0169f2995bf24a6f51402eb44f65b8e0d5cbb25
David <82801430+davesagraf@users.noreply.github.com>
2022-10-11 12:20:05 +0100

    Delete dist directory

--- dist/db/migrations/1665416315263-UserEmailPassword.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserEmailPassword1665416315263 = void 0;
-class UserEmailPassword1665416315263 {
-    async up(queryRunner) { }
-    async down(queryRunner) { }
-}
-exports.UserEmailPassword1665416315263 = UserEmailPassword1665416315263;
-//# sourceMappingURL=1665416315263-UserEmailPassword.js.map
--- dist/src/entity/user.entity.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class User {
-    id: number;
-    email: string;
-    password: string;
-    firstName: string;
-    lastName: string;
-    isActive: boolean;
-}
