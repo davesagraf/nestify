5db66d2826569a7f4a951fdb574db107fd9e3a6e
dave <mrdavidsaguev@proton.me>
2022-10-12 11:35:26 +0100

    jwt guards on all user routes

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,7 +8,9 @@ import {
   Put,
   ClassSerializerInterceptor,
   UseInterceptors,
+  UseGuards,
 } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { User } from '../entity/user.entity';
 import { UserService } from '../user/user.service';
 import { UpdateUserDto } from './dto/updateUser.dto';
@@ -17,23 +19,27 @@ import { UpdateUserDto } from './dto/updateUser.dto';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @UseGuards(JwtAuthGuard)
   @UseInterceptors(ClassSerializerInterceptor)
   @Get()
   getAllUsers(): Promise<User[]> {
     return this.userService.getAllUsers();
   }
 
+  @UseGuards(JwtAuthGuard)
   @UseInterceptors(ClassSerializerInterceptor)
   @Get('/:userId')
   getUserById(@Param('userId', ParseIntPipe) id: number): Promise<User> {
     return this.userService.getUserById(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':userId')
   deleteUser(@Param('userId') id: string): Promise<void> {
     return this.userService.deleteUser(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Put(':userId')
   update(@Param('userId') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.updateUser(+id, updateUserDto);
